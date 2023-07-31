8671c6e55efcf6782d329eedca17da76f7f74fff
Rafał Kuśmierz <r.kusmierz@stud.elka.pw.edu.pl>
2023-07-31 21:15:58 +0200

    mvmo test cec tidied

--- optimization_functions/CEC2022/mvmo_test_CEC.py
+++ optimization_functions/CEC2022/mvmo_test_CEC.py
@@ -4,7 +4,6 @@ import random
 from evolutionary_algorithms.mvmo import MVMO
 import time
 
-
 cec_optimum_dict = {
         1: 300,
         2: 400,
@@ -19,8 +18,7 @@ cec_optimum_dict = {
         11: 2600,
         12: 2700
 }
-# nx = 10  # dimensions - 2, 10, 20
-# mx = 5  # population size - int
+
 fx_n = 1  # function - 1, 2, 3, ..., 11, 12
 maxFES = 200_000
 boundaries = (-100, 100)
@@ -38,8 +36,6 @@ with open('./input_data/Rand_Seeds.txt', 'r') as handle:
         print(seeds)
         seeds = [int(float(seed[:4]) * 10 ** int(seed[-1:])) for seed in seeds]
 
-
-
 np.random.seed(seeds[int(seed_ind)])
 random.seed(seeds[int(seed_ind)])
 
@@ -48,6 +44,5 @@ mvmo_population = mvmo_optimizer.init_population(pop_size)
 start = time.time()
 mvmo_optimizer.optimize(mvmo_population, CEC.values)
 end = time.time()
-
 
 print(f'time required: {end - start}')
